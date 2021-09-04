9089b3b06b6b7debea47e9c9885f013dd4c63e45
Bria222 <briantechologies.co.ke@gmail.com>
2021-09-05 02:12:01 +0300

    styling the input form

--- task/forms.py
+++ task/forms.py
@@ -3,6 +3,7 @@ from django.forms import ModelForm
 from .models import *
 
 class TaskForm(forms.ModelForm):
+    title= forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task...'}))
     class Meta:
         model = Task
         fields = '__all__'
